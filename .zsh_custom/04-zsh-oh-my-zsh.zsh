049777686c227bc08215e4c2afe7168fe89705fe
Brenden Mersey <brendenmersey@Shuttle-Tydirium.local>
2025-11-08 12:45:14 -0800

    Update to reference to the powerlevel10k theme in the 04-zsh-oh-my-zsh.zsh module.

--- .zsh_custom/04-zsh-oh-my-zsh.zsh
+++ .zsh_custom/04-zsh-oh-my-zsh.zsh
@@ -4,14 +4,21 @@
 if [ -d "$HOME/.oh-my-zsh" ]; then
   export ZSH="$HOME/.oh-my-zsh"
 
-  # Theme
-  ZSH_THEME="powerlevel10k/powerlevel10k"
+  # Disable theme lookup (we'll source Powerlevel10k manually)
+  ZSH_THEME=""
 
   # Plugins (safe defaults)
   plugins=(git)
 
   # Load Oh My Zsh
   source $ZSH/oh-my-zsh.sh
+
+  # --- Powerlevel10k Theme (Homebrew install) ---
+  if [ -f "/usr/local/share/powerlevel10k/powerlevel10k.zsh-theme" ]; then
+    source /usr/local/share/powerlevel10k/powerlevel10k.zsh-theme
+  else
+    echo "⚠️  Powerlevel10k not found at /usr/local/share/powerlevel10k/"
+  fi
 else
   echo "⚠️  Oh My Zsh not found at $HOME/.oh-my-zsh"
 fi
